92285df3c344aba2e3b8a29a4d62ad003d020ee5
yihimin <yihimin01@gmail.com>
2025-04-03 10:28:11 +0900

    fix: comment css

--- my-app/src/components/Comment.jsx
+++ my-app/src/components/Comment.jsx
@@ -1,25 +1,46 @@
-import userIcon from '../assets/user_icon.png';
+import imgUserIcon from '../assets/user_icon.png';
 
 function Comment(props) {
   const styles = {
     wrapper: {
+      margin: 8,
       padding: 8,
       display: 'flex',
-      alignItems: 'center',
-      border: '1px solid #ccc',
-      borderRadius: 12,
+      flexDirection: 'row',
+      border: '1px solid grey',
+      borderRadius: 16,
     },
-    image: { width: 50, height: 50, borderRadius: 25, marginRight: 12 },
+    imageContainer: {},
+    image: {
+      width: 50,
+      height: 50,
+      borderRadius: 25,
+    },
+    contentContainer: {
+      marginLeft: 10,
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+    },
+    nameText: {
+      color: 'black',
+      fontSize: 16,
+      fontWeight: 'bold',
+    },
+    commentText: {
+      color: 'black',
+      fontSize: 16,
+    },
   };
 
   return (
     <div style={styles.wrapper}>
-      <img src={userIcon} alt="user" style={styles.image} />
-      <div>
-        <span style={{ fontWeight: 'bold', display: 'block' }}>
-          {props.name}
-        </span>
-        <span>{props.comment}</span>
+      <div style={styles.imageContainer}>
+        <img src={imgUserIcon} alt="user icon" style={styles.image} />
+      </div>
+      <div style={styles.contentContainer}>
+        <span style={styles.nameText}>{props.name}</span>
+        <span style={styles.commentText}>{props.comment}</span>
       </div>
     </div>
   );
