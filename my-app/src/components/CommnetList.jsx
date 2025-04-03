f5ef5488035dfb8b20a28640c03234f28f7e0579
yihimin <yihimin01@gmail.com>
2025-04-03 10:33:44 +0900

    fix: CommentList

--- my-app/src/components/CommnetList.jsx
+++ my-app/src/components/CommnetList.jsx
@@ -5,6 +5,7 @@ function CommentList() {
     <div>
       <Comment name="홍길동" comment="안녕하세요!" />
       <Comment name="유재석" comment="리액트 재밌어요!" />
+      <Comment name="강민경" comment="리액트 배워보고 싶어요!!" />
     </div>
   );
 }
