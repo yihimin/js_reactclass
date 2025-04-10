24de722ee3fc5052a5c14857fa23c8ae6b857dca
yihimin <yihimin01@gmail.com>
2025-04-10 10:28:56 +0900

    count state

--- 4w/src/index.js
+++ 4w/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import TodoList from './components/TodoList';
+import TodoList from './components/todo/TodoList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
