41b4094d83a34dbff747c9ed5309d6ebe015d8db
yihimin <yihimin01@gmail.com>
2025-04-17 10:30:29 +0900

    isRunning

--- 4w/src/components/Counter.jsx
+++ 4w/src/components/Counter.jsx
@@ -1,34 +1,23 @@
 import React, { useState, useEffect } from 'react';
 
 function Counter(props) {
-  //let count = 0;
-  // state 선언
   const [count, setCount] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
 
   useEffect(() => {
-    document.title = `${count}번 클릭`;
-    console.log(`${count}번 클릭`);
-  }, [count]);
+    if (!isRunning) return;
+    setTimeout(() => setCount((prev) => prev + 1), 1000);
+  }, [count, isRunning]);
 
   return (
     <div>
       <p>{count}</p>
       <button
         onClick={() => {
-          setCount(count - 1); // 상태를 업데이트!
-          console.log(count); // 콘솔에는 이전 값이 찍히는 점 주의!
+          setIsRunning(!isRunning);
         }}
       >
-        -1
-      </button>
-      <button
-        onClick={() => {
-          setCount((prev) => prev + 1);
-          setCount((prev) => prev + 1); // 상태를 업데이트!
-          console.log(count); // 콘솔에는 이전 값이 찍히는 점 주의!
-        }}
-      >
-        +2
+        {isRunning ? 'STOP' : 'START'}
       </button>
     </div>
   );
