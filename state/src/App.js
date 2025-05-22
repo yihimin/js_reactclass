77678af5f621db1d5cf061ff163e4351b4956286
yihimin <yihimin01@gmail.com>
2025-05-22 10:46:49 +0900

    url접속 에러 처리

--- state/src/App.js
+++ state/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
 import ProductDetail from './components/ProductDetail';
+import Error from './components/Error';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="*" element={<Error />} />
         <Route path="/product/:category/:id" element={<ProductDetail />} />
       </Routes>
     </BrowserRouter>
---
+++ state/src/components/Error.jsx
@@ -0,0 +1,3 @@
+export default function Error() {
+  return <h1>접속할 수 없는 url입니다</h1>;
+}
